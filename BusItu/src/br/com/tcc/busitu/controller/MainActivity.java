301f2a2427560401c5b30eb4a21964122a6b7f0e
marco <marcocbarbieri@gmail.com>
2014-08-18 18:09:02 -0300

    adicionado novo método de obter localização (google play services)

--- BusItu/src/br/com/tcc/busitu/controller/MainActivity.java
+++ BusItu/src/br/com/tcc/busitu/controller/MainActivity.java
@@ -1,12 +1,22 @@
 package br.com.tcc.busitu.controller;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
 import br.com.tcc.busitu.R;
 import br.com.tcc.busitu.R.layout;
 import br.com.tcc.busitu.model.TabsPagerAdapter;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -21,21 +31,28 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity implements
-		ActionBar.TabListener {
+		ActionBar.TabListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener{
 	
-	
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
 	
 	private ViewPager viewPager;
 	private TabsPagerAdapter mAdapter;
 	private ActionBar actionBar;
-	private LocationManager locationManager;
-	private TextView txtDestino;
-	String locationProvider;
+	//private LocationManager locationManager;
+	private EditText txtDestino;
+	private TextView lblLocalizacao;
+	
+	//String locationProvider;
+	private LocationClient mLocationClient;
+    Location mCurrentLocation;
 	
 	
 	
@@ -59,14 +76,20 @@ public class MainActivity extends FragmentActivity implements
 		
 		case R.id.action_map:
 			
-			Toast.makeText(this, "definindo a localização", Toast.LENGTH_LONG).show();
-			Location localLocation = locationManager.getLastKnownLocation(locationProvider);
-			Log.i("AndroidRuntime", "latitude: " + localLocation.getLatitude() + " Longitude: " + localLocation.getLongitude());
+			//Toast.makeText(this, "definindo a localização", Toast.LENGTH_LONG).show();
+			if(servicesConnected()){
+				mCurrentLocation = mLocationClient.getLastLocation();
+				
+				Toast.makeText(this, "lat: " + mCurrentLocation.getLatitude() + " long: " + mCurrentLocation.getLongitude(), Toast.LENGTH_LONG).show();
+				Log.i("AndroidRuntime", "latitude: " + mCurrentLocation.getLatitude() + " Longitude: " + mCurrentLocation.getLongitude());
+				//Location localLocation = locationManager.getLastKnownLocation(locationProvider);
+				//Log.i("AndroidRuntime", "latitude: " + localLocation.getLatitude() + " Longitude: " + localLocation.getLongitude());
 			
+				return true;
+				
+			}
 			
-			
-			
-			return true;
+
 		case R.id.action_preferences:
 			return true;
 		case R.id.action_search:
@@ -77,56 +100,31 @@ public class MainActivity extends FragmentActivity implements
 		}
 		
 		};
-		
 	
+	    @Override
+	protected void onStart() {
+	     super.onStart();
+	     // Connect the client.
+	     mLocationClient.connect();
+	}	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-	//	Context context = this.getApplicationContext();
+		mLocationClient = new LocationClient(this, this, this);
 		
-		locationManager = (LocationManager) this.getSystemService(getApplicationContext().LOCATION_SERVICE);
-		locationProvider = LocationManager.GPS_PROVIDER;
+		//locationManager = (LocationManager) this.getSystemService(getApplicationContext().LOCATION_SERVICE);
+		//locationProvider = LocationManager.GPS_PROVIDER;
 		
-/*		
-		LocationListener locationListener = new LocationListener() {
-			
-			@Override
-			public void onStatusChanged(String provider, int status, Bundle extras) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProviderEnabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProviderDisabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onLocationChanged(Location location) {
-				// TODO Auto-generated method stub
-				location.getLatitude();
-				location.getLongitude();
-				
-				
-			}
-		};
-		
-		locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);*/
 		
 		viewPager = (ViewPager) findViewById(R.id.pager);
 		actionBar = getActionBar();
 		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
 		
-		txtDestino = (TextView) findViewById(R.id.textFim);
+		txtDestino = (EditText) findViewById(R.id.textFim);
+		lblLocalizacao = (TextView) findViewById(R.id.lblInicio);
 		
 
 		
@@ -164,7 +162,12 @@ public class MainActivity extends FragmentActivity implements
         });
 	}
 	
-
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+	
 	@Override
 	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
 		// TODO Auto-generated method stub
@@ -184,10 +187,148 @@ public class MainActivity extends FragmentActivity implements
 		
 	}
 
+	// Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+    
+    /*
+     * Handle results returned to the FragmentActivity
+     * by Google Play services
+     */
+    @Override
+    protected void onActivityResult( int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+            
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode) {
+                    case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+                    
+                    break;
+                }
+            
+        }
+     }
 	
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =  GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    resultCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                        new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getFragmentManager(),"Location Updates");
+            }
+            
+            return false;
+        }
+    }
+    
+
+
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		// TODO Auto-generated method stub
+		 /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            showDialog(connectionResult.getErrorCode());
+        }
+    
+		
+	}
 	
-
-
-
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		
+	}
+	
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+		
+	}
    
 }
